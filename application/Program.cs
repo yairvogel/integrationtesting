34f11875fadaf17b0e0e8df2bb426212972e8149
Yair Vogel <yairvogel@github.com>
2024-06-13 22:12:54 +0200

    paging tests + clean up for integrration tests

--- application/Program.cs
+++ application/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+using application;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,34 +16,10 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-var blogEndpoints = app.MapGroup("blogs");
-blogEndpoints.MapGet("/", (SomeDbContext db) => db.Blogs.ToListAsync());
-
-blogEndpoints.MapPost("/", async ([FromBody] BlogRequest blog, SomeDbContext db) =>
-{
-    var entity = new Blog { Title = blog.Title };
-    await db.AddAsync(entity);
-    await db.SaveChangesAsync();
-    return Results.Created($"/blogs/{entity.BlogId}", new BlogResponse(entity.BlogId, entity.Title));
-});
-
-blogEndpoints.MapGet("/{id}", async (int id, SomeDbContext db, ILogger<Program> logger) =>
-{
-    logger.LogInformation($"got request for id {id}");
-    Blog? blog = await db.Blogs.Where(b => b.BlogId == id).FirstOrDefaultAsync();
-    return blog is null ? Results.NotFound() : Results.Ok(new BlogResponse(blog.BlogId, blog.Title));
-});
-
-blogEndpoints.MapDelete("/{id}", async (int id, SomeDbContext db) =>
-{
-    await db.Blogs.Where(b => b.BlogId == id).ExecuteDeleteAsync();
-    return Results.Ok();
-});
+app.MapBlogs();
 
 app.Run();
 
-public record BlogRequest(string Title);
-public record BlogResponse(int Id, string Title);
 
 public class SomeDbContext(DbContextOptions options) : DbContext(options)
 {
--- integrationTests/UnitTest1.cs
+++ integrationTests/UnitTest1.cs
@@ -1,47 +1,124 @@
 namespace integrationTests;
 
+using System.Collections;
 using System.Net;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Xunit.Abstractions;
+using static System.Net.HttpStatusCode;
 
-public class UnitTest1(WebApplicationFactory<Program> factory, ITestOutputHelper output) : IClassFixture<WebApplicationFactory<Program>>
+public class UnitTest1(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
 {
-    [Fact]
-    public async Task HelloWorld()
-    {
-        using var client = factory.CreateClient();
-
-        using var response = await client.GetAsync("/");
-
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-    }
-
     [Fact]
     public async Task ShouldCreateAPost()
     {
         using var client = factory.CreateClient();
         
-        string guid = Guid.NewGuid().ToString();
+        Guid guid = Guid.NewGuid();
 
-        int? id = null;
-        using (var response1 = await client.PostAsync("/blogs", JsonContent.Create(new { Title = guid })))
-        {
-            response1.StatusCode.Should().Be(HttpStatusCode.Created);
-            BlogResponse? blog = await response1.Content.ReadFromJsonAsync<BlogResponse>();
-            blog!.Title.Should().Be(guid);
-            id = blog.Id;
-        }
+        var blog = await CreateBlogAsync(client, guid);
 
-        output.WriteLine($"got id {id}");
-
-        using var response2 = await client.GetAsync($"/blogs/{id}");
-        response2.StatusCode.Should().Be(HttpStatusCode.OK);
-        (await response2.Content.ReadFromJsonAsync<BlogResponse>())!.Title.Should().Be(guid);
-
-        using var response3 = await client.DeleteAsync($"/blogs/{id}");
-        response3.StatusCode.Should().Be(HttpStatusCode.OK);
+        using var response2 = await client.GetAsync($"/blogs/{blog.Id}");
+        response2.StatusCode.Should().Be(OK);
+        (await response2.Content.ReadFromJsonAsync<BlogResponse>())!.Should().Be(blog);
+        await DeleteBlogAsync(blog, client);
     }
 
+    [Fact]
+    public async Task ShouldPaginateCorrectly()
+    {
+        using var client = factory.CreateClient();
+        List<BlogResponse> blogs = [];
+
+        foreach (int i in 0..5)
+        {
+            blogs.Add(await CreateBlogAsync(client, Guid.NewGuid()));
+        }
+
+        using var response1 = await client.GetAsync("/blogs?pageSize=1");
+        response1.StatusCode.Should().Be(OK);
+        var page1 = await response1.Content.ReadFromJsonAsync<Page<BlogResponse>>();
+        
+        using var response2 = await client.GetAsync(page1!.next);
+        response2.StatusCode.Should().Be(OK);
+        var page2 = await response2.Content.ReadFromJsonAsync<Page<BlogResponse>>();
+
+        page1.data.Count.Should().Be(1);
+        page2!.data.Count.Should().Be(1);
+
+        page2.data[0].Id.Should().BeGreaterThan(page1.data[0].Id);
+
+        await Task.WhenAll(blogs.Select(b => DeleteBlogAsync(b, client)));
+    }
+
+    [Fact]
+    public async Task ShouldUpdateABlog()
+    {
+        using var client = factory.CreateClient();
+        var guid = Guid.NewGuid();
+        var blog = await CreateBlogAsync(client, guid);
+
+        var newGuid = Guid.NewGuid();
+
+        using var put = await client.PutAsJsonAsync($"/blogs/{blog.Id}", blog with { Title = newGuid.ToString() });
+        put.StatusCode.Should().Be(OK);
+        var putBlog = await put.Content.ReadFromJsonAsync<BlogResponse>();
+
+        putBlog!.Title.Should().Be(newGuid.ToString());
+        putBlog.Id.Should().Be(blog.Id);
+
+        using var get = await client.GetAsync($"/blogs/{blog.Id}");
+        get.StatusCode.Should().Be(OK);
+        var getBlog = await get.Content.ReadFromJsonAsync<BlogResponse>();
+
+        getBlog.Should().Be(putBlog);
+
+        await DeleteBlogAsync(blog, client);
+    }
+
+    private static async Task<BlogResponse> CreateBlogAsync(HttpClient client, Guid guid)
+    {
+        var title = guid.ToString();
+        using var res = await client.PostAsync("/blogs", JsonContent.Create(new { Title = title }));
+        res.StatusCode.Should().Be(HttpStatusCode.Created);
+        BlogResponse? blog = await res.Content.ReadFromJsonAsync<BlogResponse>();
+
+        blog!.Title.Should().Be(title);
+        blog.Id.Should().BePositive();
+        return blog;
+    }
+
+    private static async Task DeleteBlogAsync(BlogResponse blog, HttpClient client)
+    {
+        using var res = await client!.DeleteAsync($"/blogs/{blog.Id}");
+        res.StatusCode.Should().Be(OK);
+    }
+
+
     private record BlogResponse(int Id, string Title);
+
+    private record Page<T>(IReadOnlyList<T> data, string next);
+}
+
+static class RangeExtensions
+{
+    public static IEnumerator<int> GetEnumerator(this Range range) => new RangeEnumerator(range);
+
+    private class RangeEnumerator(Range range) : IEnumerator<int>
+    {
+        public int Current { get; private set; } = range.Start.Value;
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose() { }
+
+        public bool MoveNext()
+        {
+            return ++Current < range.End.Value;
+        }
+
+        public void Reset()
+        {
+            Current = range.Start.Value;
+        }
+    }
 }
